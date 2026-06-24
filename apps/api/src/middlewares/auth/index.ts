16fecd79d022230d3acc853cdf230ec2aa802a62
lizardkinglk <chansanfdo@gmail.com>
2026-06-24 09:54:27 +0000

    build(resolved build lint issues): resolved lint issues with added workflow change

--- apps/api/src/middlewares/auth/index.ts
+++ apps/api/src/middlewares/auth/index.ts
@@ -1,14 +1,16 @@
 import { Request, Response, NextFunction } from 'express';
 import { getAuth } from '@clerk/express';
 
-export function requireApiAuth(req: Request, res: Response, next: NextFunction) {
+export function requireApiAuth(
+    req: Request,
+    res: Response,
+    next: NextFunction): void {
     const authState = getAuth(req);
+
     if (!authState.userId) {
         res.status(401).json({
-            error: 'Unauthorized',
-            message: 'error. you must provide a valid authentication token to access this endpoint.'
+            error: 'Unauthorized'
         });
-
         return;
     }
 
